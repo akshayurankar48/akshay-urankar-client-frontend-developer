b6b346f55bf9fbce4b36ab402a6517b1c5836ed4
evilieswithin <evilieswithin>
2023-11-16 14:12:39 +0530

    convert jsx to tsx

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="bg-dark h-screen w-screen overflow-y-scroll overflow-x-hidden scrollbar-hide">
+      {/* Navigation Bar */}
+      <Navbar />
+
+      {/* Image Slider */}
+
+      {/* Search Component */}
+
+      {/* Display Cards */}
+
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
