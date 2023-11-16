14cf62edc62e36d8df17e6a35add2743f3534fe1
evilieswithin <evilieswithin>
2023-11-16 15:14:56 +0530

    create formatDate custom function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+// Utility function for merging class names using clsx and tailwind-merge
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// Utility function for formatting a timestamp into a localized date string
+export function formatDate(timestamp: string): string {
+  // Convert the timestamp to a Date object
+  const date = new Date(timestamp);
+
+  // Options for formatting the date
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  // Use the Date.toLocaleDateString method to create a formatted date string
+  return date.toLocaleDateString('en-GB', options);
 }
