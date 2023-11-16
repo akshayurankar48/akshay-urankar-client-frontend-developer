c2c758f4bae2e7698c35074d44448929927b55d3
evilieswithin <evilieswithin>
2023-11-16 15:00:40 +0530

    use app context in App.tsx for user login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useContext } from 'react';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Slider from './components/slider/Slider';
+import { AppContext } from './store/AppContext';
 
 function App() {
+  const { isLoggedIn } = useContext(AppContext);
   return (
     <div className="bg-dark h-screen w-screen overflow-y-scroll overflow-x-hidden scrollbar-hide">
       {/* Navigation Bar */}
-      <Navbar />
+      <Navbar isLoggedIn={isLoggedIn} />
 
       {/* Image Slider */}
       <Slider />
