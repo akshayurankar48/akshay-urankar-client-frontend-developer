6f4cf975865bb5dfec6290e28e549fd3ec84cc28
evilieswithin <evilieswithin>
2023-11-16 15:00:18 +0530

    implement logout functionality

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,30 @@
 // Import MobileNav component for mobile navigation
+import { Button } from '../ui/button';
 import MobileNav from './MobileNav';
 
 // Navbar component represents the website navigation bar
-const Navbar = () => {
+const Navbar = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
+  const logout = () => {
+    localStorage.removeItem('sessionToken');
+    window.location.reload();
+  };
+
   return (
     // Navigation bar container with styling
     <nav className="flex items-center justify-between fixed z-50 w-full gap-5 bg-gray-900 p-6 sm:px-12">
       {/* Link to home with Spacex logo */}
       <a href="/">
-        <img
-          src="/assets/icons/spacex.svg"
-          alt="Spacex Logo"
-          className="w-40"
-        />
+        <img src="/assets/spacex.svg" alt="Spacex Logo" className="w-40" />
       </a>
+
+      {isLoggedIn && (
+        <Button
+          onClick={logout}
+          className="bg-dark text-light w-24 font-bold text-lg"
+        >
+          Logout
+        </Button>
+      )}
 
       {/* MobileNav component for mobile navigation */}
       <MobileNav />
